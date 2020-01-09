426df995293bb009e7617445378f9a2dc161fc4c
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2020-01-09 11:48:49 -0700

    Add location validation using regex

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -11,6 +11,24 @@ class Form extends Component {
     }
   }
 
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleLocationSubmit = (e) => {
+    e.preventDefault();
+    this.checkLocation(this.state.location);
+  }
+
+  checkLocation = (location) => {
+    const regex = /([A-Za-z]+(?: [A-Za-z]+)*),? ([A-Za-z]{2})/;
+    if (location.match(regex)) {
+      console.log(true);
+    } else {
+      console.log(false);
+    }
+  }
+
 
   render() {
     return (
@@ -22,26 +40,26 @@ class Form extends Component {
             value={this.state.username}
             name="username"
             type="text"
-            onChange={(event) => this.handleChange(event)}
+            onChange={(e) => this.handleChange(e)}
           />
           <label className="input-label" for="user-location">Location</label>
           <input
             className="form-input"
+            placeholder="e.g. Denver, CO"
             value={this.state.location}
             name="location"
             type="text"
-            onChange={(event) => this.handleChange(event)}
+            onChange={(e) => this.handleChange(e)}
           />
           <button
             className="form-button"
             type="button"
-            onClick={(event) => this.handleLocationSubmit(event)}
+            onClick={(e) => this.handleLocationSubmit(e)}
           >
             Begin the Hunt
           </button>
         </form>
       </main>
-
     )
   }
 }
