9c1f0eeb45fdcf690cca791c849c2ea881e13fc4
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2020-01-09 10:52:29 -0700

    Add styling to form

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -6,6 +6,7 @@ class Form extends Component {
   constructor() {
     super();
     this.state = {
+      username: '',
       location: ''
     }
   }
@@ -14,7 +15,31 @@ class Form extends Component {
   render() {
     return (
       <main className="form-container">
-
+        <form>
+          <label className="input-label" for="username">Ghost Hunter Name</label>
+          <input
+            className="form-input"
+            value={this.state.username}
+            name="username"
+            type="text"
+            onChange={(event) => this.handleChange(event)}
+          />
+          <label className="input-label" for="user-location">Location</label>
+          <input
+            className="form-input"
+            value={this.state.location}
+            name="location"
+            type="text"
+            onChange={(event) => this.handleChange(event)}
+          />
+          <button
+            className="form-button"
+            type="button"
+            onClick={(event) => this.handleLocationSubmit(event)}
+          >
+            Begin the Hunt
+          </button>
+        </form>
       </main>
 
     )
