01af4bee35cb54e9da3d488d8bb036aee030a7db
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2020-01-11 11:11:44 -0700

    Add buttons and styling to Card components

--- src/Components/LocationCard/LocationCard.js
+++ src/Components/LocationCard/LocationCard.js
@@ -5,7 +5,15 @@ export const LocationCard = ({ location, description }) => {
 
   return (
     <aside className="card">
+    <div className="card-title-container">
       <h3 className="card-title">{location}</h3>
+
+      <div className="card-button-container">
+        <button className="card-button">Add to Wishlist</button>
+        <button className="card-button">View Map</button>
+      </div>
+      
+    </div>
       <p className="card-description">{description}</p>
     </aside>
   )
