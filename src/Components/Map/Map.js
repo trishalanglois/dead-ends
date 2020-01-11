b6b88ef9b746832e65a206fd5235b24013aa1e9e
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2020-01-11 14:49:35 -0700

    Add overflow scroll to locations cards

---
+++ src/Components/Map/Map.js
@@ -0,0 +1,13 @@
+import { GoogleApiWrapper } from 'google-maps-react';
+import React, { Component } from 'react';
+
+
+export class Map extends Component {
+
+
+
+}
+
+export default GoogleApiWrapper({
+  apiKey: AIzaSyBFQ1czovRlzvB4l3QxC5YwoxJ_URDKP6g
+})(Map)
