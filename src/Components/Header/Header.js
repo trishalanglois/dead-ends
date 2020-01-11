741838f5b53afb9539ba5f8409734f2eb8c731ae
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2020-01-11 13:04:37 -0700

    Add Link to header to route home

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Header.scss';
+import { Link } from 'react-router-dom';
+
 
 const Header = () => {
   return (
     <nav className="header">
-      <h1 className="main-header">DEAD ENDS</h1>
+      <Link to={'/'}>
+        <h1 className="main-header">DEAD ENDS</h1>
+      </Link>
       <div className="header-line"></div>
     </nav>
   )
