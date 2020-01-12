3225c0a624b83d094026d46c0dc80d9b5a3aa365
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2020-01-12 14:35:53 -0700

    Add index.js to ignore when testing and add Snapshot for App testing

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe('App', () => {
+  it('should match the snapshot', () => {
+    let wrapper = shallow(<App />)
+
+    expect(wrapper).toMatchSnapshot();
+  })
+})
